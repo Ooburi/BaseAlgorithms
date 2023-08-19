fe824fe8ff0487f9079f6ed440c8dd9e62441916
Pavel Andreevich <paandu.da@gmail.com>
2023-08-20 03:06:46 +0900

    Добавьте файлы проекта.

---
+++ BaseAlgorithms/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BaseAlgorithms
+{
+    internal class Program
+    {
+        // Name and description of an algorithm
+        static void Main(string[] args)
+        {
+            // example of using an algorithm
+        }
+
+        static void Algorithm()
+        {
+
+        }
+    }
+}
